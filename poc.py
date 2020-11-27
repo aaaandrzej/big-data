9f74b9247fba1eb442eca3330d7add4a3a70e715
andrzej <andrzej.szulc@gmail.com>
2020-11-27 17:02:08 +0100

    initial poc for parsing data from sample sales report csv file

---
+++ poc.py
@@ -0,0 +1,46 @@
+import csv
+from dataclasses import dataclass
+from datetime import datetime
+import os
+
+
+@dataclass
+class SalesRecord:
+    region: str
+    country: str
+    item_type: str
+
+
+def timer(func):
+    def wraps():
+        start = datetime.now()
+        func()
+        finish = datetime.now()
+        print(f'execution time: {finish - start} [h:mm:ss.milliseconds]')
+        return
+
+    return wraps
+
+
+directory = 'input-data'
+
+
+@timer
+def print_csv_rows_converted_to_objects(directory=directory):
+    counter = 0
+    with open(os.path.join(directory, '5m Sales Records.csv'), newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader)  # skipping the header row
+        for row in reader:
+            region, country, item_type, *remaining = row
+            sales_record = SalesRecord(region, country, item_type)
+            print(sales_record)
+            counter += 1
+            if counter >= 1000:
+                break
+    stats = f'\n{counter} lines printed\n'
+    print(stats)
+    return
+
+
+print_csv_rows_converted_to_objects()
