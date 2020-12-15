e9f67460aeec383e548e0d1a74693454d0fdaadc
andrzej <andrzej.szulc@gmail.com>
2020-12-15 17:11:24 +0100

    multiprocessing implemented, to refactor further

--- app/poc_pandas_mp.py
+++ app/poc_pandas_mp.py
@@ -9,7 +9,7 @@ from app.adapters.load_csv import load_csv
 from app.adapters.save_df import save_df
 from app.bl.airport_matching import assign_nearest_airports
 from app.bl.df_manipulations import filter_positive_population_cities, update_df_with_country
-# from app.core.timer import timer
+from app.core.timer import timer
 from config import INPUT_FILE, OUTPUT_FILE, INPUT_FIELDNAMES, INTERIM_FIELDNAMES, OUTPUT_FIELDNAMES, \
     AIRPORTS_FILE, CC_FILE, CC_FIELDNAMES, CC_FIELDNAMES_TRIMMED
 
@@ -24,40 +24,49 @@ def df_chunking(df: pd.DataFrame, chunksize: int) -> pd.DataFrame:  # be careful
         df.drop(df.index[:chunksize], inplace=True)
 
 
-# @timer
+@timer
 def func_no_mp(nrows=None):
-    # country_info = load_csv(CC_FILE, CC_FIELDNAMES, CC_FIELDNAMES_TRIMMED, skiprows=50)
+    country_info = load_csv(CC_FILE, CC_FIELDNAMES, CC_FIELDNAMES_TRIMMED, skiprows=50)
 
     df = load_csv(INPUT_FILE, INPUT_FIELDNAMES, INTERIM_FIELDNAMES, nrows=nrows)
+    df = filter_positive_population_cities(df)
+    df = update_df_with_country(df, country_info)
+    df = df.sort_values('population', ascending=False).head(20)
 
     airports = load_csv(AIRPORTS_FILE, delimiter=',')
 
-    # assign_nearest_airports_from_hardcoded_list = partial(assign_nearest_airports, airports=airports)
+    assign_nearest_airports_from_hardcoded_list = partial(assign_nearest_airports, airports=airports)
 
-    assign_nearest_airports(df, airports)
+    assign_nearest_airports_from_hardcoded_list(df)
 
     return df
 
 
-# @timer
+@timer
 def func_mp(nrows=None):
-    # country_info = load_csv(CC_FILE, CC_FIELDNAMES, CC_FIELDNAMES_TRIMMED, skiprows=50)
+    country_info = load_csv(CC_FILE, CC_FIELDNAMES, CC_FIELDNAMES_TRIMMED, skiprows=50)
 
     df = load_csv(INPUT_FILE, INPUT_FIELDNAMES, INTERIM_FIELDNAMES, nrows=nrows)
-
-    chunks = [chunk for chunk in df_chunking(df, 100)]
+    df = filter_positive_population_cities(df)
+    df = update_df_with_country(df, country_info)
+    df = df.sort_values('population', ascending=False).head(20)
 
     airports = load_csv(AIRPORTS_FILE, delimiter=',')
 
-    with mp.Pool(2) as pool:
+    n_jobs = 2
+    n_chunks = len(df) // n_jobs
+
+    chunks = [chunk for chunk in df_chunking(df, n_chunks)]
+
+    with mp.Pool(n_jobs) as pool:
         chunks_modified = pool.map(partial(assign_nearest_airports, airports=airports), chunks)
 
     return pd.concat(chunks_modified)
 
 
 if __name__ == '__main__':
-    a = func_no_mp(1000)
-    # b = func_mp(1000)
+    a = func_no_mp()
+    # b = func_mp()
 
     # df = filter_positive_population_cities(df)
 
