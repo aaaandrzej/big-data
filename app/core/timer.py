35d10c4efc26c7bbc6d7844c3dfd6a573c3742e9
andrzej <andrzej.szulc@gmail.com>
2020-12-16 18:14:38 +0100

    revert changes

--- app/core/timer.py
+++ app/core/timer.py
@@ -1,11 +1,11 @@
-# from datetime import datetime
-#
-#
-# def timer(func):
-#     def wraps(*args, **kwargs):
-#         start = datetime.now()
-#         result = func(*args, **kwargs)
-#         finish = datetime.now()
-#         print(f'{func.__name__} function execution time: {finish - start} [h:mm:ss.milliseconds]')
-#         return result
-#     return wraps
+from datetime import datetime
+
+
+def timer(func):
+    def wraps(*args, **kwargs):
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        finish = datetime.now()
+        print(f'{func.__name__} function execution time: {finish - start} [h:mm:ss.milliseconds]')
+        return result
+    return wraps
