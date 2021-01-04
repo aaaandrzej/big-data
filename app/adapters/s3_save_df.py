8dfb089e04ae3d0edba5cc9a4342c01718ac0f45
andrzej <andrzej.szulc@gmail.com>
2021-01-04 19:02:35 +0100

    s3fs removed, load/save based on boto3 now

--- app/adapters/s3_save_df.py
+++ app/adapters/s3_save_df.py
@@ -1,15 +1,30 @@
 import os
+from io import StringIO
 
-import s3fs  # TODO pick either s3fs or boto3 to ensure consistency  # TODO consider dropping s3fs as causes conflicts
+import boto3
+
+
+# import s3fs  # TODO pick either s3fs or boto3 to ensure consistency  # TODO consider dropping s3fs as causes conflicts
 
 # from app.core.timer import timer
 
 
 # @timer
+# def s3_save_df(df, filename, columns):
+#     s3 = s3fs.S3FileSystem(anon=False, client_kwargs={'endpoint_url': os.getenv('ENDPOINT_URL')})
+#
+#     with s3.open(filename, 'w') as f:
+#         df.to_csv(f, columns=columns, index=True)
+#
+#     return
+
+
 def s3_save_df(df, filename, columns):
-    s3 = s3fs.S3FileSystem(anon=False, client_kwargs={'endpoint_url': os.getenv('ENDPOINT_URL')})
+    s3 = boto3.resource('s3',
+                        endpoint_url=os.getenv('ENDPOINT_URL'),
+                        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 
-    with s3.open(filename, 'w') as f:
-        df.to_csv(f, columns=columns, index=True)
-
-    return
+    csv_buffer = StringIO()
+    df.to_csv(csv_buffer, columns=columns, index=True)
+    s3.Object(f'{filename.parent}', f'{filename.name}').put(Body=csv_buffer.getvalue())
